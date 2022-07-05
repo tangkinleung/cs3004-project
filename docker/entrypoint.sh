08cdcd33a797e67a0f16e6c87c82512a2fa63bbd
LE0riginZ <74258545+LE0riginZ@users.noreply.github.com>
2022-07-05 18:53:39 +0800

    added docker stuff
    docker files and scripts added

---
+++ docker/entrypoint.sh
@@ -0,0 +1,13 @@
+# Change to the correct directory
+cd /usr/src/app;
+# Run hardhat
+yarn start:local;
+# Keep node alive
+set -e
+if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
+  set -- node "$@"
+fi
+exec "$@"
+
+#Start server 
+npm run dev;
